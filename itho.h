3b64548d0ebee5db22d53b56f109cfb9a7bd35db
Jan de Groot <jan@duracom.nl>
2024-02-20 21:55:50 +0100

    Add Orcon support from ESPEasy Plugin 118
    Simplify configuration by defining srcID/destID.
    Make it work with modified ESPEasy itho-lib version

--- itho.h
+++ itho.h
@@ -12,6 +12,17 @@
 // 23 -Itho to high speed with hardware timer (20 min)
 // 33 -Itho to high speed with hardware timer (30 min)
 
+// 100 - set Orcon ventilation unit to standby
+// 101 - set Orcon ventilation unit to low speed
+// 102 - set Orcon ventilation unit to medium speed
+// 103 - set Orcon ventilation unit to high speed
+// 104 - set Orcon ventilation unit to Auto
+// 110 - set Orcon to standby with hardware timer (12 hours)
+// 111 - set Orcon to low speed with hardware timer (1 hour)
+// 112 - set Orcon to medium speed with hardware timer (13 hours)
+// 113 - set Orcon to high speed with hardware timer (1 hour)
+// 114 - set Orcon to AutoCO2 mode
+
 typedef struct { String Id; String Roomname; } IdDict;
 
 // Global struct to store Names, should be changed in boot call,to set user specific
@@ -19,7 +30,10 @@ IdDict Idlist[] = { {"ID1", "Controller Room1"},
 					{"ID2",	"Controller Room2"},
 					{"ID3",	"Controller Room3"}
 				};
-IthoCC1101 rf;
+uint8_t srcID[3];
+uint8_t destID[3];
+
+IthoCC1101 *rf;
 void ITHOinterrupt() IRAM_ATTR;
 void ITHOcheck();
 
@@ -44,6 +58,10 @@ bool InitRunned = false;
 #define Time2      20*60
 #define Time3      30*60
 
+#define OrconTime0 (12*60*60)
+#define OrconTime1 (60*60)
+#define OrconTime2 (13*60*60)
+#define OrconTime3 (60*60)
 
 TextSensor *InsReffanspeed; // Used for referencing outside FanRecv Class
 
@@ -51,15 +69,21 @@ String TextSensorfromState(int currentState)
 {
 	switch (currentState)
 	{
-	case 1: 
+        case 100:
+                return "Standby";
+                break;
+	case 1: case 101:
 		return "Low";
 		break;
-	case 2:
+	case 2: case 102:
 		return "Medium";
 		break;
-	case 3: 
+	case 3: case 103:
 		return "High";
 		break;
+        case 104:
+                return "Auto";
+                break;
 	case 13: case 23: case 33:
 		return "High(T)";
 		break;
@@ -90,11 +114,13 @@ class FanRecv : public PollingComponent {
     void setup()
 	{
 	  InsReffanspeed = this->fanspeed; // Make textsensor outside class available, so it can be used in Interrupt Service Routine
-	  rf.init();
+          rf = new IthoCC1101(15, 12);
+          rf->enableOrcon(true);
+	  rf->init();
       // Followin wiring schema, change PIN if you wire differently
-      pinMode(D1, INPUT);
-      attachInterrupt(D1, ITHOinterrupt, FALLING);
-      rf.initReceive();
+      pinMode(5, INPUT);
+      attachInterrupt(5, ITHOinterrupt, FALLING);
+      rf->initReceive();
 	  InitRunned = true;
     }
     void loop() override
@@ -107,14 +133,19 @@ class FanRecv : public PollingComponent {
 	}
     void update() override
 	{
-		if (State >= 10)
+		if ((State >= 10) && (Timer > 0))
 		{
 			Timer--;
+			if (Timer == 0) { Timer--; }
 		}
 
-		if ((State >= 10) && (Timer <= 0))
+		if ((State >= 10) && (Timer < 0))
 		{
-			State = 1;
+			if (State < 100) {
+				State = 1;
+			} else {
+				State = 101;
+			}
 			Timer = 0;
 			fantimer->publish_state(String(Timer).c_str()); // this ensures that times value 0 is published when elapsed
 		}
@@ -142,14 +173,12 @@ public:
 
 	void write_state(bool state) override {
 		if (state) {
-			noInterrupts();
-			rf.sendCommand(IthoLow);
-			interrupts();
-			rf.initReceive();
+			rf->sendCommand(IthoLow);
+			rf->initReceive();
 			State = 1;
 			Timer = 0;
 			LastID = Mydeviceid;
-			publish_state(!state);
+			//publish_state(!state);
 		}
 	}
 };
@@ -159,14 +188,12 @@ class FanSendMedium : public Component, public Switch {
 
     void write_state(bool state) override {
       if ( state ) {
-		noInterrupts();  
-        rf.sendCommand(IthoMedium);
-		interrupts();
-		rf.initReceive();
+        rf->sendCommand(IthoMedium);
+		rf->initReceive();
         State = 2;
         Timer = 0;
 		LastID = Mydeviceid;
-        publish_state(!state);
+        //publish_state(!state);
       }
     }
 };
@@ -176,14 +203,12 @@ public:
 
 	void write_state(bool state) override {
 		if (state) {
-			noInterrupts();
-			rf.sendCommand(IthoHigh);
-			interrupts();
-			rf.initReceive();
+			rf->sendCommand(IthoHigh);
+			rf->initReceive();
 			State = 3;
 			Timer = 0;
 			LastID = Mydeviceid;
-			publish_state(!state);
+			//publish_state(!state);
 		}
 	}
 };
@@ -193,14 +218,12 @@ public:
 
 	void write_state(bool state) override {
 		if (state) {
-			noInterrupts();
-			rf.sendCommand(IthoFull);
-			interrupts();
-			rf.initReceive();
+			rf->sendCommand(IthoFull);
+			rf->initReceive();
 			State = 4;
 			Timer = 0;
 			LastID = Mydeviceid;
-			publish_state(!state);
+			//publish_state(!state);
 		}
 	}
 };
@@ -210,14 +233,12 @@ class FanSendIthoTimer1 : public Component, public Switch {
 
     void write_state(bool state) override {
       if ( state ) {
-		noInterrupts();  
-        rf.sendCommand(IthoTimer1);
-		interrupts();
-		rf.initReceive();
+        rf->sendCommand(IthoTimer1);
+		rf->initReceive();
         State = 13;
         Timer = Time1;
 		LastID = Mydeviceid;
-        publish_state(!state);
+        //publish_state(!state);
       }
     }
 };
@@ -227,14 +248,12 @@ class FanSendIthoTimer2 : public Component, public Switch {
 
     void write_state(bool state) override {
       if ( state ) {
-		noInterrupts();  
-        rf.sendCommand(IthoTimer2);
-		interrupts();
-		rf.initReceive();
+        rf->sendCommand(IthoTimer2);
+		rf->initReceive();
         State = 23;
         Timer = Time2;
 		LastID = Mydeviceid;
-        publish_state(!state);
+        //publish_state(!state);
       }
     }
 };
@@ -244,14 +263,162 @@ class FanSendIthoTimer3 : public Component, public Switch {
 
     void write_state(bool state) override {
       if ( state ) {
-		noInterrupts();  
-        rf.sendCommand(IthoTimer3);
-		interrupts();
-		rf.initReceive();
+        rf->sendCommand(IthoTimer3);
+		rf->initReceive();
         State = 33;
         Timer = Time3;
 		LastID = Mydeviceid;
-        publish_state(!state);
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconStandBy : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconStandBy, srcID, destID);
+        State = 100;
+        Timer = 0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconLow : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconLow, srcID, destID);
+        State = 101;
+        Timer = 0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconMedium : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconMedium, srcID, destID);
+        State = 102;
+        Timer = 0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconHigh : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconHigh, srcID, destID);
+        State = 103;
+        Timer = 0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconAuto : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconAuto, srcID, destID);
+        State = 104;
+        Timer = 0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconTimer0 : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconTimer0, srcID, destID);
+        State = 110;
+        Timer = OrconTime0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconTimer1 : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconTimer1, srcID, destID);
+        State = 111;
+        Timer = OrconTime1;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconTimer2 : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconTimer2, srcID, destID);
+        State = 112;
+        Timer = OrconTime2;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconTimer3 : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconTimer3, srcID, destID);
+        State = 113;
+        Timer = OrconTime3;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
+      }
+    }
+};
+
+class FanSendOrconAutoCO2 : public Component, public Switch {
+  public:
+
+    void write_state(bool state) override {
+      if ( state ) {
+        rf->sendCommand(OrconAutoCO2, srcID, destID);
+        State = 114;
+        Timer = 0;
+        LastID = Mydeviceid;
+        rf->initReceive();
+        //publish_state(!state);
       }
     }
 };
@@ -261,10 +428,8 @@ class FanSendIthoJoin : public Component, public Switch {
 
     void write_state(bool state) override {
       if ( state ) {
-		noInterrupts();  
-        rf.sendCommand(IthoJoin);
-		interrupts();
-		rf.initReceive();
+        rf->sendCommand(IthoJoin);
+		rf->initReceive();
         State = 1111;
         Timer = 0;
         publish_state(!state);
@@ -290,10 +455,9 @@ int RFRemoteIndex(String rfremoteid)
 
 
 void ITHOcheck() {
-  noInterrupts(); // disable new interrupts
-  if (rf.checkForNewPacket()) {
-    IthoCommand cmd = rf.getLastCommand();
-	String Id = rf.getLastIDstr();
+  if (rf->checkForNewPacket()) {
+    IthoCommand cmd = rf->getLastCommand();
+	String Id = rf->getLastIDstr();
 	int index = RFRemoteIndex(Id);
 	if ( index>=0) { // Only accept commands that are in the list
 		switch (cmd) {
@@ -349,11 +513,70 @@ void ITHOcheck() {
 			break;
 		case IthoLeave:
 			break;
+                case OrconStandBy:
+                        ESP_LOGD("custom", "Orcon Standby");
+                        State = 100;
+                        Timer = 0;
+                        LastID = Idlist[index].Roomname;
+			break;
+                case OrconLow:
+                        ESP_LOGD("custom", "Orcon Low");
+                        State = 101;
+                        Timer = 0;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconMedium:
+                        ESP_LOGD("custom", "Orcon Medium");
+                        State = 102;
+                        Timer = 0;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconHigh:
+                        ESP_LOGD("custom", "Orcon High");
+                        State = 103;
+                        Timer = 0;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconAuto:
+                        ESP_LOGD("custom", "Orcon Auto");
+                        State = 104;
+                        Timer = 0;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconTimer0:
+                        ESP_LOGD("custom", "Orcon Timer0");
+                        State = 110;
+                        Timer = OrconTime0;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconTimer1:
+                        ESP_LOGD("custom", "Orcon Timer1");
+                        State = 111;
+                        Timer = OrconTime1;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconTimer2:
+                        ESP_LOGD("custom", "Orcon Timer2");
+                        State = 112;
+                        Timer = OrconTime2;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconTimer3:
+                        ESP_LOGD("custom", "Orcon Timer3");
+                        State = 113;
+                        Timer = OrconTime3;
+                        LastID = Idlist[index].Roomname;
+                        break;
+                case OrconAutoCO2:
+                        ESP_LOGD("custom", "Orcon Auto CO2");
+                        State = 114;
+                        Timer = 0;
+                        LastID = Idlist[index].Roomname;
+                        break;
 		default:
 		    break;
 		}
 	}
 	else ESP_LOGV("","Ignored device-id: %s", Id.c_str());
   }
-  interrupts(); //enable interrupts
 }
